39eabf4feedc587d5c5dfb1a7ce34fce7ccda9ee
vinylhousegarage <vinylhousegarage@gmail.com>
2025-02-04 15:29:18 +0900

    Decouple root route from __init__.py by using main Blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,11 @@
 from flask import Flask
 from .config import Config
 from .db import db
+from .routes.main import main_bp
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
     db.init_app(app)
-
-    @app.route("/")
-    def index():
-        try:
-            db.session.execute("SELECT 1")
-            return "Database connection successful!"
-        except Exception as e:
-            return f"Database connection failed: {str(e)}", 500
-
+    app.register_blueprint(main_bp)
     return app
