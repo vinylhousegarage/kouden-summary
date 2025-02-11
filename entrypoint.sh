a58e2f93463f719febbdb8782baeaefc85696b04
vinylhousegarage <vinylhousegarage@gmail.com>
2025-02-11 13:10:53 +0900

    Add initial entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+if [ ! -d "migrations" ]; then
+    echo "Initializing database migration..."
+    flask db init
+    flask db migrate -m "Initial migration"
+fi
+
+echo "Applying database migrations..."
+flask db upgrade
+
+echo "Starting Gunicorn..."
+exec gunicorn -b 0.0.0.0:5000 app:create_app()
