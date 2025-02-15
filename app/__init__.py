7336e5fc9634624d6dbcfa43e8fa6b236e1957f9
vinylhousegarage <vinylhousegarage@gmail.com>
2025-02-15 10:23:19 +0900

    Add health check endpoint for ALB in __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -3,10 +3,12 @@ from .config import Config
 from .db import db, init_db
 from .routes.main import main_bp
 from . import models
+from app.routes.health import health_bp
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
     init_db(app)
     app.register_blueprint(main_bp)
+    app.register_blueprint(health_bp)
     return app
