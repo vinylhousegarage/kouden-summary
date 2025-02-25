11d5f1275095e023b7452a677af11cb93cdb6838
vinylhousegarage <vinylhousegarage@gmail.com>
2025-02-25 14:02:09 +0900

    Change health check endpoint to 'health_check' for correct routing

--- app/middleware/before_request.py
+++ app/middleware/before_request.py
@@ -3,9 +3,7 @@ from flask import session, redirect, url_for, request
 def require_login(app):
     @app.before_request
     def _require_login():
-        app.logger.debug(f"Before request - request.endpoint: {request.endpoint}")
-
-        if request.endpoint in ["health.health", "main.login", "main.authorize"]:
+        if request.endpoint in ["health.health_check", "main.login", "main.authorize"]:
             return
 
         if "user" not in session:
