6665c578cda201901ad885231fa5c9d49fb8fd5b
vinylhousegarage <vinylhousegarage@gmail.com>
2025-03-03 17:46:48 +0900

    Encode COGNITO_SCOPE for url generate in auth_helpers.py

--- app/utils/auth_helpers.py
+++ app/utils/auth_helpers.py
@@ -1,12 +1,17 @@
 import requests
+import urllib
 from flask import current_app
 from app.config import Config
 
 def generate_cognito_login_url():
+    encoded_scope = urllib.parse.quote(Config.COGNITO_SCOPE)
+
     return (
         f"{Config.COGNITO_DOMAIN}/login?"
-        f"client_id={Config.COGNITO_CLIENT_ID}&response_type=code&"
-        f"scope=openid+email&redirect_uri={Config.COGNITO_REDIRECT_URI}"
+        f"client_id={Config.COGNITO_CLIENT_ID}&"
+        f"response_type=code&"
+        f"scope={encoded_scope}&"
+        f"redirect_uri={Config.COGNITO_REDIRECT_URI}"
     )
 
 def send_cognito_token_request(grant_type, extra_data):
