ff9585121cd694af3d6969a042b7ab5bd93d3872
vinylhousegarage <vinylhousegarage@gmail.com>
2025-03-17 09:53:41 +0900

    Add created_at and updated_at timestamps

--- app/models.py
+++ app/models.py
@@ -1,4 +1,3 @@
-from sqlalchemy.sql import func
 from app.extensions import db
 
 class Summary(db.Model):
@@ -11,5 +10,5 @@ class Summary(db.Model):
     tel = db.Column(db.String(20), nullable=True, info={'label': '電話'})
     note = db.Column(db.String(250), nullable=True, info={'label': '備考'})
     user_cognito_id = db.Column(db.String(36), nullable=False)
-    created_at = db.Column(db.DateTime, nullable=False, default=func.now())
-    updated_at = db.Column(db.DateTime, nullable=False, default=func.now(), onupdate=func.now())
+    created_at = db.Column(db.DateTime, nullable=False, server_default=db.func.current_timestamp())
+    updated_at = db.Column(db.DateTime, nullable=False, server_default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
