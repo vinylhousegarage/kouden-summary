248e6e2756f7531d0394e695d5782fe56ec6bd9b
vinylhousegarage <vinylhousegarage@gmail.com>
2025-03-18 10:15:40 +0900

    Refactor: Move database_reset to utils/database_helpers

---
+++ app/utils/database_helpers.py
@@ -0,0 +1,17 @@
+from app.extensions import db
+from app.models import Summary
+
+def database_reset():
+    try:
+        db.session.query(Summary).delete()
+        db.session.commit()
+
+        db.session.execute("ALTER SEQUENCE summary_id_seq RESTART WITH 1;")
+        db.session.commit()
+
+        return True
+
+    except Exception as e:
+        db.session.rollback()
+        print(f"Error: {e}")
+        return False
