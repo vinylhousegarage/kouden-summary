d75d3eb691999f6285a5ea3d0d28189c10dd6465
vinylhousegarage <vinylhousegarage@gmail.com>
2025-03-24 14:07:23 +0900

    Add index page, routing, and dashboard link

--- app/routes/main.py
+++ app/routes/main.py
@@ -1,6 +1,5 @@
 from flask import Blueprint, session, render_template
 from app.utils.auth_helpers import redirect_to_cognito_login
-from app.models import Summary
 from app.forms import CSRFForm
 
 main_bp = Blueprint('main', __name__)
@@ -10,7 +9,5 @@ def main():
     if 'access_token' not in session:
         return redirect_to_cognito_login()
 
-    summaries = Summary.query.all()
     form = CSRFForm()
-
-    return render_template('dashboard.html', summaries=summaries, form=form)
+    return render_template('dashboard.html', form=form)
