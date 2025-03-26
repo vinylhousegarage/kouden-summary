427c2bdb432da0d3bb2812cc05eb3080d9c9f5ce
vinylhousegarage <vinylhousegarage@gmail.com>
2025-03-26 18:07:21 +0900

    Add tel input validation to forms.py

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,6 @@
+from wtforms import StringField, HiddenField
+from wtforms.validators import Regexp, DataRequired
 from flask_wtf import FlaskForm
-from wtforms import HiddenField
-from wtforms.validators import DataRequired
 from wtforms_alchemy import ModelForm
 from app.models import Summary
 
@@ -8,6 +8,10 @@ class SummaryForm(FlaskForm, ModelForm):
     class Meta:
         model = Summary
         exclude = ['user_cognito_id', 'created_at', 'updated_at']
+
+    tel = StringField('電話', validators=[
+        Regexp(r'^\d*$', message='数字のみで入力してください')
+    ])
 
 class DeleteForm(FlaskForm):
     id = HiddenField('ID', validators=[DataRequired()])
