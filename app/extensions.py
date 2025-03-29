65c53c2b6616999564d4c83d9c4f8da07360b734
vinylhousegarage <vinylhousegarage@gmail.com>
2025-03-29 19:02:04 +0900

    Remove cognito_auth from __init__.py and extensions.py

--- app/extensions.py
+++ app/extensions.py
@@ -1,7 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_wtf import CSRFProtect
-from flask_cognito_lib import CognitoAuth
 from authlib.integrations.flask_client import OAuth
 from flask_session import Session
 from app.utils.encrypted_serializer import EncryptedSessionSerializer
@@ -10,7 +9,6 @@ from app.config import Config
 db = SQLAlchemy()
 migrate = Migrate()
 csrf = CSRFProtect()
-cognito_auth = CognitoAuth()
 oauth = OAuth()
 session = Session()
 session.serializer = EncryptedSessionSerializer(Config.FERNET_KEY)
