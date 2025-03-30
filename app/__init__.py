7011239ec319354448ae4197b191512629f4ceb7
vinylhousegarage <vinylhousegarage@gmail.com>
2025-03-30 11:22:36 +0900

    Remove unused init_oauth in __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -3,13 +3,12 @@ from app.config import Config
 from app.logging_config import check_existing_handlers, setup_logging
 from app.middleware.request_logging import setup_request_logging
 from app.extensions import csrf, migrate, db, session
-from app.oauth import init_oauth
 from app.utils.db_setup import ensure_mediumblob
 from app.routes.main import main_bp
 from app.routes.health import health_bp
 from app.routes.auth import auth_bp
 from app.routes.summaries import summaries_bp
-from .middleware.context_processors import register_context_processors
+from app.middleware.context_processors import register_context_processors
 from app.middleware.before_request import require_login
 
 def create_app():
@@ -24,8 +23,6 @@ def create_app():
     setup_request_logging(app)
 
     csrf.init_app(app)
-
-    init_oauth(app)
 
     db.init_app(app)
     migrate.init_app(app, db)
