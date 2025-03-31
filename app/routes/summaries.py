1d1502d591684bc0d3b626f9aff75be5aa11884a
vinylhousegarage <vinylhousegarage@gmail.com>
2025-03-31 18:10:12 +0900

    Chore: reorder imports in summaries.py

--- app/routes/summaries.py
+++ app/routes/summaries.py
@@ -1,5 +1,4 @@
-from flask import (Blueprint, current_app, flash, redirect, render_template,
-                   request, session, url_for)
+from flask import (Blueprint, current_app, flash, redirect, render_template, request, session, url_for)
 
 from app.extensions import db
 from app.forms import CSRFForm, DeleteForm, SummaryForm
