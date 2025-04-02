acd50e4299af8f0e5fdec0d1bd3137c659645a29
vinylhousegarage <vinylhousegarage@gmail.com>
2025-04-02 16:55:53 +0900

    Refactor decode_cognito_jwt in jwt_helpers.py: add issuer verification; remove unnecessary access_token attribute

--- app/utils/jwt_helpers.py
+++ app/utils/jwt_helpers.py
@@ -36,7 +36,7 @@ def get_public_key_from_jwk(jwks, kid):
     )
     return pem_key
 
-def decode_cognito_jwt(id_token, access_token):
+def decode_cognito_jwt(id_token):
     jwks = get_cognito_jwk()
     headers = jwt.get_unverified_header(id_token)
     kid = headers['kid']
@@ -47,7 +47,7 @@ def decode_cognito_jwt(id_token, access_token):
         pem_key,
         algorithms=['RS256'],
         audience=Config.AWS_COGNITO_USER_POOL_CLIENT_ID,
-        access_token=access_token,
+        issuer=f'https://cognito-idp.{Config.AWS_REGION}.amazonaws.com/{Config.AWS_COGNITO_USER_POOL_ID}'
     )
     return claims
 
