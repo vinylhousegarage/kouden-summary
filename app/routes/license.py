0f02d8503c8ff1660d6beb79343870e71b6de7d4
vinylhousegarage <vinylhousegarage@gmail.com>
2025-04-06 14:05:42 +0900

    refactor(license): replace license() with license_page() in license.py

--- app/routes/license.py
+++ app/routes/license.py
@@ -3,5 +3,5 @@ from flask import Blueprint, render_template
 license_bp = Blueprint('license', __name__)
 
 @license_bp.route('/license')
-def license():
+def license_page():
     return render_template('license.html')
