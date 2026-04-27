9d71355914dcd8d327cd4b58f0820fe60ccef11f
vinylhousegarage <vinylhousegarage@gmail.com>
2026-04-27 15:18:18 +0900

    chore(terraform): adds S3 backend configuration

--- terraform/remove/ec2_green.tf
+++ terraform/remove/ec2_green.tf
@@ -1,9 +1,17 @@
-# 1. AWSプロバイダーの設定
+terraform {
+  backend "s3" {
+    bucket = "kouden-summary-tfstate"
+    key    = "remove/terraform.tfstate"
+    region = "ap-northeast-1"
+  }
+}
+
+# AWSプロバイダーの設定
 provider "aws" {
   region = "ap-northeast-1"
 }
 
-# 3. セキュリティグループ (インスタンス用)
+# 関連セキュリティグループ
 resource "aws_security_group" "flask_ec2_sg" {
   name        = "flask-ec2-sg"
   description = "Flask EC2 Security Group"
@@ -12,12 +20,9 @@ resource "aws_security_group" "flask_ec2_sg" {
   tags = {
     Name = "SG-EC2-Instance"
   }
-
-  # インバウンド・アウトバウンドのルールは長くなるため
-  # インポート後に terraform show で確認するのが効率的です
 }
 
-# 4. IAM インスタンスプロファイル
+# IAM インスタンスプロファイル
 resource "aws_iam_instance_profile" "ecs_instance_role" {
   name = "ecsInstanceRole"
   role = "ecsInstanceRole"
