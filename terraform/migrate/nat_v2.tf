42cf7b13e6c87a18c93e7dd25d0d8e033a5ae4dd
vinylhousegarage <vinylhousegarage@gmail.com>
2026-04-28 11:10:15 +0900

    update(terraform): upgrade NAT instance to AL2023 v2.1.0

--- terraform/migrate/nat_v2.tf
+++ terraform/migrate/nat_v2.tf
@@ -9,26 +9,44 @@ data "aws_ami" "al2023" {
   }
 }
 
-# NATインスタンス (v2) の作成
+# NATインスタンス (v2) の更新
 resource "aws_instance" "nat_v2" {
   # 基本スペック
   ami           = data.aws_ami.al2023.id
   instance_type = "t2.micro"
   key_name      = "instance-key-002"
 
-  # ネットワーク・セキュリティ
+  # ネットワーク・セキュリティ設定
   subnet_id              = "subnet-0de9867b55c955be2"
   vpc_security_group_ids = ["sg-0cbab0718ed76d5db"]
-  source_dest_check      = false
 
-  # 権限・セキュリティ設定
+  # NAT機能・権限設定
+  source_dest_check    = false
   iam_instance_profile = "NAT-Instance-Role"
+
+  # マスカレード（NAT）設定
+  user_data = <<-EOT
+    #!/bin/bash
+    echo "net.ipv4.ip_forward = 1" > /etc/sysctl.d/99-nat.conf
+    sysctl -p /etc/sysctl.d/99-nat.conf
+    systemctl enable --now nftables
+    nft add table ip nat
+    nft add chain ip nat postrouting { type nat hook postrouting priority 100 \; }
+    nft add rule ip nat postrouting oifname "eth0" counter masquerade
+    nft list ruleset > /etc/nftables/main.nft
+    echo 'include "/etc/nftables/main.nft"' >> /etc/sysctl.conf
+  EOT
+
+  # 既存インスタンスを削除し新規作成
+  user_data_replace_on_change = true
+
+  # IMDSv2設定
   metadata_options {
     http_tokens   = "required"
     http_endpoint = "enabled"
   }
 
-  # ストレージ
+  # ストレージ設定
   root_block_device {
     volume_type = "gp3"
     volume_size = 8
@@ -40,6 +58,12 @@ resource "aws_instance" "nat_v2" {
   tags = {
     Name    = "nat-instance-v2"
     OS      = "AL2023"
-    Version = "v2.0.0"
+    Version = "v2.1.0"
   }
+}
+
+# パブリックIP設定
+resource "aws_eip" "nat_eip" {
+  instance = aws_instance.nat_v2.id
+  domain   = "vpc"
 }
