827688400981280fc4e4e797e3f80ec900ff62f5
vinylhousegarage <vinylhousegarage@gmail.com>
2026-04-28 15:46:23 +0900

    cleanup(terraform): add legacy NAT instance resources for removal

---
+++ terraform/remove/nat.tf
@@ -0,0 +1,40 @@
+# 1. State管理（S3）
+terraform {
+  backend "s3" {
+    bucket = "kouden-summary-tfstate"
+    key    = "remove/terraform.tfstate"
+    region = "ap-northeast-1"
+  }
+}
+
+# 2. AWSプロバイダー設定
+provider "aws" {
+  region = "ap-northeast-1"
+}
+
+# 3. AL2NATインスタンス
+resource "aws_instance" "nat_instance" {
+  ami           = "ami-022282eb775c6a4fa"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "nat-instance"
+  }
+}
+
+# 4. 関連セキュリティグループ
+resource "aws_security_group" "flask_nat_instance_sg" {
+  name        = "flask-nat-instance-sg"
+  description = "Allow private subnet instances to access the internet via NAT"
+  vpc_id      = "vpc-09a84d9fab986d185"
+
+  tags = {
+    Name = "SG-NAT-Instance"
+  }
+}
+
+# 5. IAM インスタンスプロファイル
+resource "aws_iam_instance_profile" "NAT_Instance_Role" {
+  name = "NAT-Instance-Role"
+  role = "NAT-Instance-Role"
+}
