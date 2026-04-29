a15c7a82f487df6be827c73f7ec58fcb9fd379e3
vinylhousegarage <vinylhousegarage@gmail.com>
2026-04-29 10:05:00 +0900

    refactor(terraform): set root terraform.tfstate as the primary backend

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -4,7 +4,7 @@ terraform {
 
   backend "s3" {
     bucket = "kouden-summary-tfstate"
-    key    = "remove/terraform.tfstate"
+    key    = "terraform.tfstate"
     region = "ap-northeast-1"
   }
 
