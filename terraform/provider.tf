9ee4ef59254192324908becb11ea0c3add17132b
vinylhousegarage <vinylhousegarage@gmail.com>
2026-04-29 09:48:58 +0900

    chore(terraform): setup s3 backend and import configuration for ECS

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,6 +1,13 @@
 # 動作環境の固定
 terraform {
   required_version = "~> 1.14"
+
+  backend "s3" {
+    bucket = "kouden-summary-tfstate"
+    key    = "remove/terraform.tfstate"
+    region = "ap-northeast-1"
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
