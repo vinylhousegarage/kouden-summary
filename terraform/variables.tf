13a5af5708d46d64d774aefd39e7e07d64d89667
vinylhousegarage <vinylhousegarage@gmail.com>
2026-05-03 09:57:47 +0900

    feat(codedeploy): define variables for ECS Blue/Green deployment

---
+++ terraform/variables.tf
@@ -0,0 +1,6 @@
+variable "alb_production_listener_arn" { type = string }
+variable "alb_test_listener_arn" { type = string }
+variable "ecs_cluster_arn" { type = string }
+variable "ecs_cluster_name" { type = string }
+variable "initial_task_definition_arn" { type = string }
+variable "vpc_id" { type = string }
