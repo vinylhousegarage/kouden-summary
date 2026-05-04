44f4aa282671f25ae0923c9a2fdc2c7ad879cc1d
vinylhousegarage <vinylhousegarage@gmail.com>
2026-05-05 06:10:07 +0900

    refactor(ecs): remove unused target group for rolling update

--- terraform/generated_ec2_v2.tf
+++ terraform/generated_ec2_v2.tf
@@ -2,53 +2,6 @@
 # Please review these resources and move them into your main configuration files.
 
 # __generated__ by Terraform
-resource "aws_lb_target_group" "target_group_blue" {
-  deregistration_delay               = "300"
-  ip_address_type                    = "ipv4"
-  lambda_multi_value_headers_enabled = null
-  load_balancing_algorithm_type      = "round_robin"
-  load_balancing_anomaly_mitigation  = "off"
-  load_balancing_cross_zone_enabled  = "use_load_balancer_configuration"
-  name                               = "flask-tg-blue"
-  port                               = 5000
-  protocol                           = "HTTP"
-  protocol_version                   = "HTTP1"
-  proxy_protocol_v2                  = null
-  slow_start                         = 0
-  tags                               = {}
-  tags_all                           = {}
-  target_type                        = "instance"
-  vpc_id                             = "vpc-09a84d9fab986d185"
-  health_check {
-    enabled             = true
-    healthy_threshold   = 3
-    interval            = 30
-    matcher             = "200"
-    path                = "/health"
-    port                = "traffic-port"
-    protocol            = "HTTP"
-    timeout             = 10
-    unhealthy_threshold = 2
-  }
-  stickiness {
-    cookie_duration = 86400
-    cookie_name     = null
-    enabled         = false
-    type            = "lb_cookie"
-  }
-  target_group_health {
-    dns_failover {
-      minimum_healthy_targets_count      = "1"
-      minimum_healthy_targets_percentage = "off"
-    }
-    unhealthy_state_routing {
-      minimum_healthy_targets_count      = 1
-      minimum_healthy_targets_percentage = "off"
-    }
-  }
-}
-
-# __generated__ by Terraform
 resource "aws_lb" "alb" {
   client_keep_alive                           = 3600
   customer_owned_ipv4_pool                    = null
